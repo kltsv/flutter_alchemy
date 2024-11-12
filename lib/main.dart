7307544a465ad6f12303e255d689e144ab35c5f0
Sergey Koltsov <srg.koltsov@yandex.ru>
2024-11-12 20:58:13 +0300

    Add simple GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const _initialItems = ['вода', 'огонь', 'воздух', 'земля'];
+
 void main() {
   runApp(const App());
 }
@@ -11,10 +13,29 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Alchemy',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello world'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  static const _paddings = 12.0;
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: _paddings,
+          crossAxisSpacing: _paddings,
         ),
+        itemCount: _initialItems.length,
+        itemBuilder: (context, index) {
+          return Container(height: 100, width: 100, color: Colors.blue);
+        },
       ),
     );
   }
