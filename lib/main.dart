b14c40f9b0a812ed2979bd73a6e6f1f57f02e912
Sergey Koltsov <srg.koltsov@yandex.ru>
2024-11-12 21:36:29 +0300

    Paddings and circle styling for items

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,36 @@ class App extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   static const _paddings = 12.0;
+  static const _color = Colors.blue;
 
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          mainAxisSpacing: _paddings,
-          crossAxisSpacing: _paddings,
+      body: Padding(
+        padding: const EdgeInsets.all(_paddings),
+        child: GridView.builder(
+          reverse: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            mainAxisSpacing: _paddings,
+            crossAxisSpacing: _paddings,
+          ),
+          itemCount: _initialItems.length,
+          itemBuilder: (context, index) {
+            final item = _initialItems[index];
+            return Container(
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: _color,
+              ),
+              child: Center(child: Text(item)),
+            );
+          },
         ),
-        itemCount: _initialItems.length,
-        itemBuilder: (context, index) {
-          return Container(height: 100, width: 100, color: Colors.blue);
-        },
       ),
     );
   }
