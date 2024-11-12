fda65b034740140fb073bdecf759eeca63a1e74b
Sergey Koltsov <srg.koltsov@yandex.ru>
2024-11-12 21:49:14 +0300

    [01] Basic UI (#1)
    * Add simple GridView
    
    * Paddings and circle styling for items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const _initialItems = ['вода', 'огонь', 'воздух', 'земля'];
+
 void main() {
   runApp(const App());
 }
@@ -11,9 +13,42 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Alchemy',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello world'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  static const _paddings = 12.0;
+  static const _color = Colors.blue;
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(_paddings),
+        child: GridView.builder(
+          reverse: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            mainAxisSpacing: _paddings,
+            crossAxisSpacing: _paddings,
+          ),
+          itemCount: _initialItems.length,
+          itemBuilder: (context, index) {
+            final item = _initialItems[index];
+            return Container(
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: _color,
+              ),
+              child: Center(child: Text(item)),
+            );
+          },
         ),
       ),
     );
